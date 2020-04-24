45e9cacdb27b189416d7a009ad3abe458dffcb2d
Reyleig <50756544+Reyleig@users.noreply.github.com>
2020-04-23 22:26:23 -0500

    comit

--- compra/Pagos.php
+++ compra/Pagos.php
@@ -85,13 +85,13 @@ $custRow = $query->fetch_assoc();
     </table>
     <div class="shipAddr">
         <h4>Detalles de envío</h4>
-        <p><?php echo $custRow['name']; ?></p>
-        <p><?php echo $custRow['email']; ?></p>
-        <p><?php echo $custRow['phone']; ?></p>
-        <p><?php echo $custRow['address']; ?></p>
+        <p><?php echo @$_SESSION["name"]; ?></p>
+        <p><?php echo @$_SESSION["email"]; ?></p>
+        <p><?php echo @$_SESSION["phone"]; ?></p>
+        <p><?php echo @$_SESSION["address"]; ?></p>
     </div>
     <div class="footBtn">
-        <a href="index.php" class="btn btn-warning"><i class="glyphicon glyphicon-menu-left"></i> Continue Comprando</a>
+        <a href="template1.php" class="btn btn-warning"><i class="glyphicon glyphicon-menu-left"></i> Continue Comprando</a>
         <a href="AccionCarta.php?action=placeOrder" class="btn btn-success orderBtn">Realizar pedido <i class="glyphicon glyphicon-menu-right"></i></a>
     </div>
         </div>
--- verDataSesion.php
+++ verDataSesion.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 echo $_SESSION["name"];
+$_SESSION["email"]; 
+              $_SESSION["phone"]; 
+              $_SESSION["address"];
          
 ?>
    
